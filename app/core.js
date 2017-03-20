a5b4c7f7ff18ac89b06c0b47950964b87bf224f4
Federico Doll <fdoll@hexacta.com>
2017-03-20 17:04:36 -0300

    Fix Responsive behavior

--- app/core.js
+++ app/core.js
@@ -13,17 +13,17 @@ app.controller("MainController", ['$scope', '$http', function($scope, $http) {
 		$scope.selectedContact = contact;
 		window.scrollTo(0, 0);
 		if ($(window).width() < 767) {
-	        //$('#contacts').replaceWith($('#details'));
 	        $('#contacts').addClass("hidden-xs");
 	        $('#details').removeClass("hidden-xs");
 		}
+		console.log($(this));
 	}
 
 	$scope.BackToContacts = function(){
 		$scope.selectedContact = null;
-		window.scrollTo(0, 0);
 		$('#contacts').removeClass("hidden-xs");
 	    $('#details').addClass("hidden-xs");
+	    window.scrollTo(0, 0);
 	}
 
 	$(".backup-picture").on("error", function(){
