653d5603171ee2da036e24c6cde43c2f247758af
Federico Doll <fdoll@hexacta.com>
2017-03-20 12:17:34 -0300

    Fix scroll-bar and Mobile

--- app/core.js
+++ app/core.js
@@ -12,6 +12,18 @@ app.controller("MainController", ['$scope', '$http', function($scope, $http) {
 	$scope.SelectContact = function(contact){
 		$scope.selectedContact = contact;
 		window.scrollTo(0, 0);
+		if ($(window).width() < 767) {
+	        //$('#contacts').replaceWith($('#details'));
+	        $('#contacts').addClass("hidden-xs");
+	        $('#details').removeClass("hidden-xs");
+		}
+	}
+
+	$scope.BackToContacts = function(){
+		$scope.selectedContact = null;
+		window.scrollTo(0, 0);
+		$('#contacts').removeClass("hidden-xs");
+	    $('#details').addClass("hidden-xs");
 	}
 
 	$(".backup-picture").on("error", function(){
